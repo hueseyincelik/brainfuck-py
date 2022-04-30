af27432e3965bd4362f9fb9d84afa14a1eed80e1
hueseyincelik <104273133+hueseyincelik@users.noreply.github.com>
2022-04-30 22:37:50 +0200

    add brainfuck interpreter cli

---
+++ src/bf-cli.py
@@ -0,0 +1,18 @@
+import argparse, sys
+
+parser = argparse.ArgumentParser()
+p = parser.add_mutually_exclusive_group()
+
+p.add_argument('-f', '--file', help='evaluate a file')
+p.add_argument('-s', '--string', help='evaluate a string')
+
+args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
+
+if args.file:
+	with open(args.file, 'r') as file:
+		code = (file.read().replace('\n', '')).replace(' ', '')
+
+	brainfuck(code)
+
+elif args.string:
+	brainfuck((args.string.replace('\n', '')).replace(' ', ''))
